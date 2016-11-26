80d2088c0ec6d69dac34bc23524fd12c33c32f1d
spirifoxy <spirit.ixi@gmail.com>
2016-11-27 03:35:40 +0700

    Add perspective projection, translation, reflection, scaling

--- js/main.js
+++ js/main.js
@@ -1,130 +1,235 @@
 
 var ColorEnum = Object.freeze({WHITE: 0, RED: 1, GREEN: 2, BLUE: 3});
 
-	function scene()
-	{
-	    this.param = {
-	        width: 1000,
-	        height: 500,
-	        distance: -500
-	    };
-	    this.color = 'rgb(255,255,255)';
-	    this.alpha = 0.5;
-	    this.object = {};
-	}
+function scene() {
+    this.param = {
+        width: 1000,
+        height: 500,
+        distance: -500
+    };
+    this.color = 'rgb(255,255,255)';
+    this.alpha = 0.5;
+    this.object = {};
+}
 
-	var world = new scene();
+var world = new scene();
 
-	var tetaX = 0;
-	var tetaY = 0;
-	var tetaZ = 0;
+var tetaX = 0;
+var tetaY = 0;
+var tetaZ = 0;
 
-	var curColor = 0;
+var dx = 0;
+var dy = 0;
+var dz = 0;
 
-	var manualRotate = true;
+var curColor = 0;
 
-	function init()
-	{
-	    world.object = new letterA();
-	    
-	    document.onkeydown = onKeyDown;
-	    document.onkeyup = onKeyUp;
-	    setInterval(draw, 10);
-	}
+var isManualRotate = true;
+var isPerspectiveOn = true;
 
-	function onKeyDown(evt) 
-	{
-	    if(evt.keyCode==32) {
-	        curColor = (curColor+1) % 4;
-	        switch(curColor) {
-	            case ColorEnum.WHITE:
-	                world.color = 'rgb(255,255,255)';
-	                break;
-	            case ColorEnum.RED:
-	                world.color = 'rgb(255,0,0)';
-	                break;
-	            case ColorEnum.GREEN:
-	                world.color = 'rgb(0,255,0)';
-	                break;
-	            case ColorEnum.BLUE:
-	                world.color = 'rgb(0,0,255)';
-	                break;
-	        }
-	    }
+function init() {
+    world.object = new letterA();
 
-	    //блок поворота
-	    if (manualRotate) {
-	        if(evt.keyCode==87) { //w
-	            tetaX = -0.1;
-	        }
-	        if(evt.keyCode==83) { //s
-	            tetaX = 0.1;
-	        }
-	        if(evt.keyCode==68) { //d
-	            tetaY = 0.1;
-	        }
-	        if(evt.keyCode==65) { //a
-	            tetaY = -0.1;
-	        }
-	        if(evt.keyCode==81) { //q
-	            tetaZ = 0.1;
-	        }
-	        if(evt.keyCode==69) { //e
-	            tetaZ = -0.1;
-	        }
-	    }
-	}
+    document.onkeydown = onKeyDown;
+    document.onkeyup = onKeyUp;
+    document.onmousemove = onMouseMove;
 
-	function onKeyUp(evt) 
-	{
-	    if (manualRotate) {
-	        //wasd
-	        if([87,83].indexOf(evt.keyCode) > -1) { 
-	            tetaX = 0;
-	        }
-	        if([68,65].indexOf(evt.keyCode) > -1) { 
-	            tetaY = 0;
-	        }
-	        if([81,69].indexOf(evt.keyCode) > -1) { 
-	            tetaZ = 0;
-	        }
-	    }
-	}
+    var rotationModeBtn = document.getElementById("rotation-mode-btn");
+    var projectionBtn = document.getElementById("projection-btn");
 
-	
-	function draw() 
-	{
-	    var canvas = document.getElementById("display");
-	    var projTitle = document.getElementById("projection-title");
+    var reflectXBtn = document.getElementById("reflect-x");
+    var reflectYBtn = document.getElementById("reflect-y");
+    var reflectZBtn = document.getElementById("reflect-z");
 
-	    if (!canvas.getContext) 
-	        retrun;
+    var scaleXM = document.getElementById("scale-x-");
+    var scaleXP = document.getElementById("scale-x+");
+    
+    var scaleYM = document.getElementById("scale-y-");
+    var scaleYP = document.getElementById("scale-y+");
 
-	    var ctx = canvas.getContext("2d");
-	    var projectedPoints = [];
-	    
-	    ctx.clearRect(0, 0, world.param.width, world.param.height);
-	    ctx.globalAlpha = world.alpha;
-	    ctx.fillStyle = world.color;
+    var scaleZM = document.getElementById("scale-z-");
+    var scaleZP = document.getElementById("scale-z+");
 
-	    rotateObject(world.object, tetaX, tetaY, tetaZ);
+    var translateX = document.getElementById("translate-x");
+    var translateY = document.getElementById("translate-y");
+    var translateZ = document.getElementById("translate-z");
 
-	    for (var i = 0; i < world.object.points.length; i++)
-            projectedPoints[i] = orthoProject(world.param, world.object.points[i]);
-        projTitle.innerHTML = "Проекция: ортографическая";
-	    
-	    for (var i = 0; i < world.object.overlaysNumber; i++)
-	    {
-	        var vertexIndex = world.object.overlays[i];
+    rotationModeBtn.onclick = changeRotationMode;
+    projectionBtn.onclick = changeProjection;
 
-	        ctx.beginPath();
-	        ctx.moveTo(projectedPoints[vertexIndex[0]][0], projectedPoints[vertexIndex[0]][1]);
+    reflectXBtn.onclick = function() {
+        reflect(world.object, [-1,1,1]);
+    }
+    reflectYBtn.onclick = function() {
+        reflect(world.object, [1,-1,1]);
+    }
+    reflectZBtn.onclick = function() {
+        reflect(world.object, [1,1,-1]);
+    }
 
-	        for (var z = 1; z < world.object.overlays[i].length; z++)
-	            ctx.lineTo(projectedPoints[vertexIndex[z]][0], projectedPoints[vertexIndex[z]][1]);
+    scaleXM.onclick = function() {
+        scale(world.object, [0.9,1,1]);
+    }
+    scaleXP.onclick = function() {
+        scale(world.object, [1.1,1,1]);
+    }
+    scaleYM.onclick = function() {
+        scale(world.object, [1,0.9,1]);
+    }
+    scaleYP.onclick = function() {
+        scale(world.object, [1,1.1,1]);
+    }
+    scaleZM.onclick = function() {
+        scale(world.object, [1,1,0.9]);
+    }
+    scaleZP.onclick = function() {
+        scale(world.object, [1,1,1.1]);
+    }
 
-	        ctx.closePath();
-	        ctx.fill();
-	        ctx.stroke();
-	    }
-	}
+    translateX.onmousemove = function() {
+        dx = +this.value;
+    }
+    translateY.onmousemove = function() {
+        dy = +this.value;
+    }
+    translateZ.onmousemove = function() {
+        dz = +this.value;
+    }
+    translateX.onmouseup =
+        translateY.onmouseup =
+        translateZ.onmouseup =
+        function() {
+            dx = dy = dz = 0;
+            this.value = 0;
+    }
+
+    setInterval(draw, 10);
+}
+
+function onKeyDown(evt) {
+    if(evt.keyCode==32) {
+        curColor = (curColor+1) % 4;
+        switch(curColor) {
+            case ColorEnum.WHITE:
+                world.color = 'rgb(255,255,255)';
+                break;
+            case ColorEnum.RED:
+                world.color = 'rgb(255,0,0)';
+                break;
+            case ColorEnum.GREEN:
+                world.color = 'rgb(0,255,0)';
+                break;
+            case ColorEnum.BLUE:
+                world.color = 'rgb(0,0,255)';
+                break;
+        }
+    }
+
+    //блок поворота
+    if (isManualRotate) {
+        if(evt.keyCode==87) { //w
+            tetaX = -0.1;
+        }
+        if(evt.keyCode==83) { //s
+            tetaX = 0.1;
+        }
+        if(evt.keyCode==68) { //d
+            tetaY = 0.1;
+        }
+        if(evt.keyCode==65) { //a
+            tetaY = -0.1;
+        }
+        if(evt.keyCode==81) { //q
+            tetaZ = 0.1;
+        }
+        if(evt.keyCode==69) { //e
+            tetaZ = -0.1;
+        }
+    }
+}
+
+function onKeyUp(evt) {
+    if (isManualRotate) {
+        //wasd
+        if([87,83].indexOf(evt.keyCode) > -1) {
+            tetaX = 0;
+        }
+        if([68,65].indexOf(evt.keyCode) > -1) {
+            tetaY = 0;
+        }
+        if([81,69].indexOf(evt.keyCode) > -1) {
+            tetaZ = 0;
+        }
+    }
+}
+
+function onMouseMove(evt) {
+    if (isManualRotate)
+        return;
+    var x = evt.pageX - document.getElementById("display").offsetLeft;
+    var y = evt.pageY - document.getElementById("display").offsetTop;
+
+    if ((x > 0) && (x < world.param.width) 
+        && (y > 0) && (y < world.param.height)) {
+        tetaY = 0.1 * (x - world.param.width/2) / world.param.width/2;
+        tetaX = 0.1 * (y - world.param.height/2) / world.param.height/2;
+    }
+}
+
+function changeRotationMode(evt) {
+    var rotTitle = document.getElementById("rotation-title");
+    var text = "Режим вращения: ";
+    
+    isManualRotate = !isManualRotate;
+    tetaX = tetaY = tetaZ = 0;
+
+    text += isManualRotate ? "ручной" : "автоматический";
+    rotTitle.innerHTML = text;
+}
+
+function changeProjection(evt) {
+    var projTitle = document.getElementById("projection-title");
+    var text = "Проекция: ";
+
+    isPerspectiveOn = !isPerspectiveOn;
+
+    text += isPerspectiveOn ? "перспективная" : "ортографическая";
+    projTitle.innerHTML = text;
+}
+
+
+function draw() {
+    var canvas = document.getElementById("display");
+    
+    if (!canvas.getContext) 
+        retrun;
+
+    var ctx = canvas.getContext("2d");
+    var projectedPoints = [];
+    
+    ctx.clearRect(0, 0, world.param.width, world.param.height);
+    ctx.globalAlpha = world.alpha;
+    ctx.fillStyle = world.color;
+
+    rotateObject(world.object, tetaX, tetaY, tetaZ);
+    translateObject(world.object, [dx, dy, dz]);
+
+    for (var i = 0; i < world.object.points.length; i++)
+        projectedPoints[i] = isPerspectiveOn 
+            ? persProject(world.param, world.object.points[i])
+            : orthoProject(world.param, world.object.points[i]);
+
+    for (var i = 0; i < world.object.overlaysNumber; i++) {
+        var vertexIndex = world.object.overlays[i];
+
+        ctx.beginPath();
+        ctx.moveTo(projectedPoints[vertexIndex[0]][0], projectedPoints[vertexIndex[0]][1]);
+
+        for (var z = 1; z < world.object.overlays[i].length; z++)
+            ctx.lineTo(projectedPoints[vertexIndex[z]][0], projectedPoints[vertexIndex[z]][1]);
+
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    }
+}
--- js/transform.js
+++ js/transform.js
@@ -1,86 +1,152 @@
 function mulMatVec(matrix, vector) {
-	var result = [];
-	for (var i = 0; i < vector.length; i++) {
-		result[i] = 0;
-		for (var j = 0; j < vector.length; j++) {
-			result[i] += matrix[j][i] * vector[j];
-		}
-	}
-	return result;
+    var result = [];
+    for (var i = 0; i < vector.length; i++) {
+        result[i] = 0;
+        for (var j = 0; j < vector.length; j++) {
+            result[i] += matrix[j][i] * vector[j];
+        }
+    }
+    return result;
 }
 
 function rotateObject(object, angleX, angleY, angleZ) {
 
-	var center = object.center;
-	for (var k = 0; k < object.pointsNumber; k++) {
-		for (var i = 0; i < object.points[k].length; i++)
-			object.points[k][i] -= center[i]; 
-		
-		object.points[k][object.points[k].length] = 1; //h 
+    var center = object.center;
+    for (var k = 0; k < object.pointsNumber; k++) {
+        for (var i = 0; i < object.points[k].length; i++)
+            object.points[k][i] -= center[i]; 
+        
+        object.points[k][object.points[k].length] = 1; //h 
 
-		rotateX(object.points[k], angleX);
-		rotateY(object.points[k], angleY);
-		rotateZ(object.points[k], angleZ);
+        if (angleX != 0)
+            object.points[k] = rotateX(object.points[k], angleX);
+        if (angleY != 0)
+            object.points[k] = rotateY(object.points[k], angleY);
+        if (angleZ != 0)
+            object.points[k] = rotateZ(object.points[k], angleZ);
 
-		object.points[k].length = 3; // убрать h из point
+        object.points[k].length = 3; // убрать h из point
 
-		for (var i = 0; i < object.points[k].length; i++)
-			object.points[k][i] += center[i];
-	}
+        for (var i = 0; i < object.points[k].length; i++)
+            object.points[k][i] += center[i];
+    }
 }
 
 function rotateX(point, angle) {
-	var rotateMatrix = [
-		[1,0,0,0],
-		[0,Math.cos(angle),Math.sin(angle),0],	
-		[0,-Math.sin(angle),Math.cos(angle),0],
-		[0,0,0,1]
-	];
+    var rotateMatrix = [
+        [1,0,0,0],
+        [0,Math.cos(angle),Math.sin(angle),0],  
+        [0,-Math.sin(angle),Math.cos(angle),0],
+        [0,0,0,1]
+    ];
 
-	var result = mulMatVec(rotateMatrix, point);
-	for (var i = 0; i < result.length; i++)
-		point[i] = result[i];
+    return mulMatVec(rotateMatrix, point);
 }
 
 function rotateY(point, angle) {
-	var rotateMatrix = [
-		[Math.cos(angle),0,-Math.sin(angle),0],
-		[0,1,0,1],
-		[Math.sin(angle),0,Math.cos(angle),0],
-		[0,0,0,1]
-	];
+    var rotateMatrix = [
+        [Math.cos(angle),0,-Math.sin(angle),0],
+        [0,1,0,1],
+        [Math.sin(angle),0,Math.cos(angle),0],
+        [0,0,0,1]
+    ];
 
-
-	var result = mulMatVec(rotateMatrix, point);
-	for (var i = 0; i < result.length; i++)
-		point[i] = result[i];
+    return mulMatVec(rotateMatrix, point);
 }
 
 function rotateZ(point, angle) {
-	var rotateMatrix = [
-		[Math.cos(angle),Math.sin(angle),0,0],
-		[-Math.sin(angle),Math.cos(angle),0,0],
-		[0,0,1,0],
-		[0,0,0,1]
-	];
+    var rotateMatrix = [
+        [Math.cos(angle),Math.sin(angle),0,0],
+        [-Math.sin(angle),Math.cos(angle),0,0],
+        [0,0,1,0],
+        [0,0,0,1]
+    ];
 
-	var result = mulMatVec(rotateMatrix, point);
-	for (var i = 0; i < result.length; i++)
-		point[i] = result[i];
+    return mulMatVec(rotateMatrix, point);
 }
 
-function orthoProject(sceneInfo, point)
-{
-	var orthoMatrix = [
-		[1,0,0,0],
-		[0,1,0,0],
-		[0,0,1,0],
-		[0,0,0,0]
-	];
+function orthoProject(sceneInfo, point) {
+    var orthoMatrix = [
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,0]
+    ];
 
-	var result = mulMatVec(orthoMatrix, point);
-	result[0] = sceneInfo.width/2 + result[0];
-	result[1] = sceneInfo.height/2 - result[1];
+    var result = mulMatVec(orthoMatrix, point);
+    result[0] = sceneInfo.width/2 + result[0];
+    result[1] = sceneInfo.height/2 - result[1];
 
-	return result;
+    return result;
+}
+
+function persProject(sceneInfo, point) {
+    
+    point[point.length] = 1; //h 
+
+    var persMatrix = [
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,1/sceneInfo.distance],
+        [0,0,0,1]
+    ];
+
+    var result = mulMatVec(persMatrix, point);
+    
+    result[0] /= result[3];
+    result[1] /= result[3];
+    result[2] /= result[3];
+    
+    result[0] = sceneInfo.width/2 + result[0];
+    result[1] = sceneInfo.height/2 - result[1];
+
+    point.length -= 1; 
+    result.length -= 1;
+    
+    return result;
+}
+
+
+function translateObject(object, vector) {
+    translate(vector, object.center);
+    
+    for (var i = 0; i < object.pointsNumber; i++) {
+        translate(vector, object.points[i]);
+    }
+}
+
+function translate(vector, point) {
+    for (var i = 0; i < point.length; i++) {
+        point[i] += vector[i];
+    }
+}
+
+function reflect(object, vector) {
+    var refMatrix = [
+        [vector[0],0,0,0],
+        [0,vector[1],0,0],
+        [0,0,vector[2],0],
+        [0,0,0,1]
+    ];
+
+    object.center = mulMatVec(refMatrix, object.center);
+    
+    for (var i = 0; i < object.pointsNumber; i++) {
+        object.points[i] = mulMatVec(refMatrix, object.points[i]);  
+    }
+}
+
+function scale(object, vector) {
+    var scaleMatrix = [
+        [vector[0],0,0,0],
+        [0,vector[1],0,0],
+        [0,0,vector[2],0],
+        [0,0,0,1]
+    ];
+
+    object.center = mulMatVec(scaleMatrix, object.center);
+    
+    for (var i = 0; i < object.pointsNumber; i++) {
+        object.points[i] = mulMatVec(scaleMatrix, object.points[i]);
+    }
 }
